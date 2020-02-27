bb2f54823870eaac23f3254bdbbf83bb5d4472ff
liuzhenzhen <315475165@qq.com>
2020-02-27 18:17:40 +0800

    工具类提交

---
+++ src/test/java/com/zhenzhen/common/utils/StringUtilTest.java
@@ -0,0 +1,37 @@
+package com.zhenzhen.common.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+	@Test
+	public void testHasLength() {
+		String src="";
+		boolean b = StringUtil.hasLength(src);
+		System.out.println(b);
+	}
+
+	@Test
+	public void testHasText() {
+		String src="";
+		boolean b = StringUtil.hasText(src);
+		System.out.println(b);
+	}
+
+	@Test
+	public void testRandomChineseString() {
+		String string = StringUtil.randomChineseString(3);
+		System.out.println(string);
+	}
+
+	@Test
+	public void testGenerateChineseName() {
+		for (int i = 0; i < 6; i++) {
+			String name = StringUtil.generateChineseName();
+			System.out.println(name);
+		}
+	}
+
+}
