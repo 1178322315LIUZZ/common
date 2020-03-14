920213c83e174279046fbf0876d344160f454cfc
liuzhenzhen <315475165@qq.com>
2020-03-14 18:24:29 +0800

    地址判断

--- src/main/java/com/zhenzhen/common/utils/NumberUtil.java
+++ src/main/java/com/zhenzhen/common/utils/NumberUtil.java
@@ -6,7 +6,15 @@ package com.zhenzhen.common.utils;
  *
  */
 public class NumberUtil {
-	
+	/**
+	 * 
+	 * @Title: getPercent 
+	 * @Description: 百分比计算
+	 * @param current
+	 * @param total
+	 * @return
+	 * @return: int
+	 */
 	public static int getPercent(int current, int total){
 		Double doo=(double) ((float)current/(float)total)*100;
 		double as=Math.floor(doo);
